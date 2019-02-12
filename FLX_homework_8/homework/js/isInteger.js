90920beed86a1a02abbe49ade6fba58bfd4c98bc
IlGoloviy <goloviyillya@gmail.com>
2019-02-12 23:33:21 +0200

    перезапис

--- FLX_homework_8/homework/js/isInteger.js
+++
@@ -1,6 +0,0 @@
-function isInteger(a) {
-	return (a ^ 0) === a;
-}
-
-isInteger(5);
-isInteger(5.1);
--- FLX_homework_8/homework/js/reverseNumber.js
+++
@@ -1,8 +0,0 @@
-function reverseNumber(a) {
-	const reversedNum = a.toString().split('').reverse().join('');
-	return Math.sign(a) * parseInt(reversedNum);
-}
-
-reverseNumber(123);
-reverseNumber(-456);
-reverseNumber(10000);
