efc23eac8002e4a425b082fd77ffdc32a39b7e0c
IlGoloviy <goloviyillya@gmail.com>
2019-02-18 21:34:17 +0200

    edit spaces

--- FLX_homework_10/homework/src/credits_handler.js
+++ FLX_homework_10/homework/src/credits_handler.js
@@ -1,101 +1,100 @@
 function userCard(keys) {
 
-	let balance = 100;
-	let transactionLimit = 100;
-	let historyLogs = [];
-	let key = keys;
+  let balance = 100;
+  let transactionLimit = 100;
+  let historyLogs = [];
+  let key = keys;
 	
-	let getCardOptions = function () {
-		return {
-			key,
-			balance,
-			transactionLimit,
-			historyLogs
-		}
-	}
+  let getCardOptions = function () {
+  return {
+      key,
+      balance,
+      transactionLimit,
+      historyLogs
+    }
+  }
 
-	function formatTime(time) {
-		const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-		const num = 10;
-		let day = time.getDate() < num ? '0'+time.getDate() : time.getDate();
-		let hour = time.getHours() < num ? '0'+time.getHours() : time.getHours();
-		let min = time.getMinutes() < num ? '0'+time.getMinutes() : time.getMinutes();
-		let sec = time.getSeconds() < num ? '0'+time.getSeconds() : time.getSeconds();
-		let formatTime = day + '/' + month[time.getMonth()] + '/' + time.getFullYear()+', '+hour+':'+min+':'+sec;
-		return formatTime;
-	}
+  function formatTime(time) {
+    const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    const num = 10;
+    let day = time.getDate() < num ? '0'+time.getDate() : time.getDate();
+    let hour = time.getHours() < num ? '0'+time.getHours() : time.getHours();
+    let min = time.getMinutes() < num ? '0'+time.getMinutes() : time.getMinutes();
+    let sec = time.getSeconds() < num ? '0'+time.getSeconds() : time.getSeconds();
+    let formatTime = day + '/' + month[time.getMonth()] + '/' + time.getFullYear()+', '+hour+':'+min+':'+sec;
+    return formatTime;
+  }
 
-	let putCredits = function(amountCredits) {
-		balance += amountCredits;
-		historyLogs.push({
-			operationType: 'Received credits',
-			credits: amountCredits,
-			operationTime: formatTime(new Date())
-		});
-	}
+  let putCredits = function(amountCredits) {
+    balance += amountCredits;
+    historyLogs.push({
+      operationType: 'Received credits',
+      credits: amountCredits,
+      operationTime: formatTime(new Date())
+    });
+  }
 
-	let	takeCredits = function(amountCredits) {
-		if (balance >= amountCredits && transactionLimit >= amountCredits) {
-			balance = balance - amountCredits;
-		} else {
-			console.error('The amount of credits exceeds the balance of the card or the transaction limit');
-		}
-		historyLogs.push({
-			operationType: 'Withdrawal of credits',
-			credits: amountCredits,
-			operationTime: formatTime(new Date())
-		});
-	}
+  let	takeCredits = function(amountCredits) {
+    if (balance >= amountCredits && transactionLimit >= amountCredits) {
+      balance = balance - amountCredits;
+    } else {
+      console.error('The amount of credits exceeds the balance of the card or the transaction limit');
+    }
+    historyLogs.push({
+      operationType: 'Withdrawal of credits',
+      credits: amountCredits,
+      operationTime: formatTime(new Date())
+    });
+  }
 
-	let	setTransactionLimit = function(sumLimit) {
-		transactionLimit = sumLimit;
-		historyLogs.push({
-			operationType: 'Transaction limit change',
-			credits: sumLimit,
-			operationTime: formatTime(new Date())
-		});
-	}
+  let	setTransactionLimit = function(sumLimit) {
+    transactionLimit = sumLimit;
+    historyLogs.push({
+      operationType: 'Transaction limit change',
+      credits: sumLimit,
+      operationTime: formatTime(new Date())
+    });
+  }
 
-	let	transferCredits = function(sumCredits, card) {
-		const onePercent = 100, percentTaxed = 0.5;
-		let taxed = sumCredits/onePercent * percentTaxed;
-		if (balance >= sumCredits + taxed && transactionLimit >= sumCredits + taxed) {
-			balance = balance - (sumCredits + taxed);
-			card.putCredits(sumCredits);
-			historyLogs.push({
-				operationType: 'Withdrawal of credits',
-				credits: sumCredits + taxed,
-				operationTime: formatTime(new Date())
-			});
-		}
-	}
+  let	transferCredits = function(sumCredits, card) {
+    const onePercent = 100, percentTaxed = 0.5;
+    let taxed = sumCredits/onePercent * percentTaxed;
+    if (balance >= sumCredits + taxed && transactionLimit >= sumCredits + taxed) {
+      balance = balance - (sumCredits + taxed);
+      card.putCredits(sumCredits);
+      historyLogs.push({
+        operationType: 'Withdrawal of credits',
+        credits: sumCredits + taxed,
+        operationTime: formatTime(new Date())
+      });
+    }
+  }
 
-	return {
-		getCardOptions,
-		putCredits,
-		takeCredits,
-		setTransactionLimit,
-		transferCredits
-	}
-	
+  return {
+    getCardOptions,
+    putCredits,
+    takeCredits,
+    setTransactionLimit,
+    transferCredits
+  }
 }
 
 class UserAccount {
-	constructor(name) {
-		this.name = name;
-		this.cards = [];
-	}
+  constructor(name) {
+    this.name = name;
+    this.cards = [];
+  }
 
-	addCard() {
-		const maxCard = 3;
-		if (this.cards.length < maxCard) {
-			this.cards.push(userCard(this.cards.length+1));
-		}
-	}
+  addCard() {
+    const maxCard = 3;
+    if (this.cards.length < maxCard) {
+      this.cards.push(userCard(this.cards.length+1));
+    }
+  }
 
-	getCardByKey(key) {
-		return this.cards[key-1];
-	}
+  getCardByKey(key) {
+    return this.cards[key-1];
+  }
 }
 
 // let user = new UserAccount('Bob');
